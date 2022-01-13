6a8661b3f3b1bcc9a4f00baa10b8972e092c018a
dillin92 <dillin970@gmail.com>
2022-01-13 06:48:23 -0500

    updated bio to reflect Carter being born!

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ const About = () => {
         <AboutContainer>
           <h1 id="about">Who am I?</h1>        
           <p>
-                    Hello, My name is Dillin Helsley. Im 29 years old and I live in Muncie, Indiana. I have 3 beautiful kids and I am happily engaged with another baby on the way! I currently work at a grocery distribution center for Wal-Mart stores as an order filler. I am a graduate of Butler University's Executive Coding Bootcamp. I am eagerly anticipating the day that I enter into the field of web development.
+                    Hello, My name is Dillin Helsley. Im 29 years old and I live in Muncie, Indiana. I have 4 beautiful kids and I am happily engaged! I currently work at a grocery distribution center for Wal-Mart stores as an order filler. I am a graduate of Butler University's Executive Coding Bootcamp. I am eagerly anticipating the day that I enter into the field of web development.
                     In my free time I love to play music, mainly guitar,vocals, and trumpet but I can also play banjo, violin, piano, tuba, and little bit of drums. I have been a performing artist for about 10 years now. I have organized and promoted music events. I have been on the radio locally and have performed with national acts. Music has given me many memorable experiences!
           </p> 
         </AboutContainer>
